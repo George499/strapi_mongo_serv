4cae2e0441e826824992d0b917dcc39ad8d142e6
George499 <53001250+George499@users.noreply.github.com>
2021-02-24 16:22:28 +0300

    no controller

--- api/albums/controllers/albums.js
+++ api/albums/controllers/albums.js
@@ -1,22 +1,22 @@
-"use strict";
+// "use strict";
 
-const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+// const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-module.exports = {
-  /**
-   * Create a record.
-   *
-   * @return {Object}
-   */
+// module.exports = {
+//   /**
+//    * Create a record.
+//    *
+//    * @return {Object}
+//    */
 
-  async create(ctx) {
-    let entity;
-    if (ctx.is("multipart")) {
-      const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.albums.create(data, { files });
-    } else {
-      entity = await strapi.services.albums.create(ctx.request.body);
-    }
-    return sanitizeEntity(entity, { model: strapi.models.restaurant });
-  },
-};
+//   async create(ctx) {
+//     let entity;
+//     if (ctx.is("multipart")) {
+//       const { data, files } = parseMultipartData(ctx);
+//       entity = await strapi.services.albums.create(data, { files });
+//     } else {
+//       entity = await strapi.services.albums.create(ctx.request.body);
+//     }
+//     return sanitizeEntity(entity, { model: strapi.models.restaurant });
+//   },
+// };
