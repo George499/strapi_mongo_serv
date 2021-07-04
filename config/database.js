ae4ce8174abff6ed82ca4be5849e26ac62aa1db5
George499 <gi.32@mail.ru>
2021-07-04 18:37:32 +0300

    update

--- config/database.js
+++ config/database.js
@@ -4,8 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri:
-          "mongodb://admin:admin@cluster0-shard-00-00.ixl4y.mongodb.net:27017,cluster0-shard-00-01.ixl4y.mongodb.net:27017,cluster0-shard-00-02.ixl4y.mongodb.net:27017/nextjs_serv?ssl=true&replicaSet=atlas-wd2p8o-shard-0&authSource=admin&retryWrites=true&w=majority",
+        uri: "mongodb://admin:admin@cluster0-shard-00-00.ixl4y.mongodb.net:27017,cluster0-shard-00-01.ixl4y.mongodb.net:27017,cluster0-shard-00-02.ixl4y.mongodb.net:27017/nextjs_serv?ssl=true&replicaSet=atlas-wd2p8o-shard-0&authSource=admin&retryWrites=true&w=majority",
         // host: env("DATABASE_HOST", "cluster0.ixl4y.mongodb.net"),
         // srv: env.bool("DATABASE_SRV", true),
         // port: env.int("DATABASE_PORT", 27017),
