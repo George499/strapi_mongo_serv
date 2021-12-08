45c85a23014a714cb7787a146143bdc962c3a53b
The Ghost <tghostlycat@gmail.com>
2021-12-08 05:12:39 +0300

    Add basic caching to all models

---
+++ config/middleware.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+    settings: {
+      cache: {
+        enabled: true,
+        models: [
+            'albums',
+            'artists',
+            'card-lookbooks',
+            'ceo-pages',
+            'ceo-pages-groups',
+            'cities',
+            'contacts-photos',
+            'genres',
+            'metros',
+            'navigations',
+            'pages',
+            'payed-articles',
+            'posts',
+            'usligis',
+            'uslugi-groups'
+        ],
+      }
+    }
+});
